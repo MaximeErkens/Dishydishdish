0f54698611a591e962a731f4e96504bee37f7eb3
Maxime Erkens <mhmserkens@gmail.com>
2022-08-18 19:10:00 +0200

    add mongodb atlas

--- db/index.js
+++ db/index.js
@@ -5,13 +5,15 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = require("../utils/consts");
+//const MONGO_URL = require("../utils/consts");
+
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost/Dishydish";
 
 mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
+  .connect(MONGO_URL)
+  .then((connection) => {
     console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+      `Connected to Mongo! Database name: "${connection.connections[0].name}"`
     );
   })
   .catch((err) => {
